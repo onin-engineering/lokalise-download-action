e1012559cadb9e9f964617558a7a341eb2220006
Alex <alex@onin.co>
2021-02-23 14:45:11 +0000

    Fixed bad action build

--- download.sh
+++ download.sh
@@ -5,7 +5,7 @@ lokalise2 \
     --project-id $2 \
     file download \
     --format $4 \
-    --unzip-to $3
-    --bundle-structure $5
-    --original-filenames $6 
-    --placeholder-format $7
+    --unzip-to $3 \
+    --bundle-structure $5 \
+    --original-filenames $6 \
+    --placeholder-format $7 \
